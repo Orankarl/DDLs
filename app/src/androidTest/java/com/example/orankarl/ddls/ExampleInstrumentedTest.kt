9e2cda18eea149b84e733d8c3b52cc3dccaa3878
Orankarl <895168349@qq.com>
2017-12-20 15:08:01 +0800

    init commmit

---
+++ app/src/androidTest/java/com/example/orankarl/ddls/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.orankarl.ddls
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.orankarl.ddls", appContext.packageName)
+    }
+}
